5e7b19fb21324da5137ed6cb2f425bf59242c870
wwsheng009 <vincentwwsheng@gmail.com>
2024-04-24 15:57:25 +0800

    update the start script

--- init.sh
+++ init.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-yao migrate && yao run flows.demo.data
+# Path to the .env file
+ENV_FILE=".env"
+
+# Check if the .env file exists
+if [ -f "$ENV_FILE" ]; then
+    echo ".env file found. Running 'yao migrate'..."
+    # Run yao migrate
+    /usr/local/bin/yao migrate
+fi
+
+
+/usr/local/bin/yao start
